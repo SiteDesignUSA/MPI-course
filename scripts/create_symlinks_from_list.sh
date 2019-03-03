23dca30c809ba9dd89de9ee33fcda7cad2e4c4fc
SiteDesignUSA <tj@sitedesignusa.com>
2019-03-03 00:30:46 -0600

    Update create_symlinks_from_list.sh
    replaced contents of list variable

--- scripts/create_symlinks_from_list.sh
+++ scripts/create_symlinks_from_list.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
- 
+
 # This simple script creates a symlink to all comma separated targets in the
 # target directory
-DIR=/etc/puppetlabs/code/environments 
-LIST=$1
- 
+DIR=/etc/puppetlabs/code/environments
+LIST=$(find /home/ -type d -name "dev_*" -print)
+
 if ! [[ -d $DIR ]]; then
   echo "$DIR is not a valid directory. Aborting"
   exit 1
