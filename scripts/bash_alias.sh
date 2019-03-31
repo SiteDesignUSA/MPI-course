29ef691f4fd58b73121e34a90d49b6b09fe24838
SiteDesignUSA <tj@sitedesignusa.com>
2019-03-31 00:55:15 -0500

    feat: dabnm, delete all branches no merge. Not like git flow feature, release finish.

--- scripts/bash_alias.sh
+++ scripts/bash_alias.sh
@@ -16,5 +16,7 @@ alias lbit='git remote show origin'
 alias car="/usr/local/bin/checkrepos"
 #gffsa, git flow feature start all repos. Reference feature start script
 alias gffsa="/usr/local/bin/start_feature_branches"
+#dabnm, delete all branches no merge. Instead of git flow feature or release finish.
+alias dabnm="/usr/local/bin/delete_all_branch_no_merge"
 #lla, list all- even . files like .git etc.
 alias lla='ls -lah --color=auto'
