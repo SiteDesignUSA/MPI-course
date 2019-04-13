acedd053ba524179cb7e0365f3f46a8dc0c34ad3
SiteDesignUSA <tj@sitedesignusa.com>
2019-04-13 18:10:21 -0500

    Update bash_alias.sh

--- scripts/bash_alias.sh
+++ scripts/bash_alias.sh
@@ -13,13 +13,14 @@ alias lar='git remote -v'
 #lbit, Local Branch Is Tracking (Local and Remote)
 alias lbit='git remote show origin'
 #ptb, Prune Tracking Branches (Local and Remote)
-alias ptb=$(git fetch --prune && git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -d)
+alias gptb='git fetch --prune && git branch -r | awk "{print \$1}" | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk "{print \$1}" | xargs git branch -d; git remote prune cache'
 #car, Check All Repos. Reference CheckRepos script
 alias car="/usr/local/bin/checkrepos"
 #gffsa, git flow feature start all repos. Reference feature start script
 alias gffsa="/usr/local/bin/start_feature_branches"
 #gffsa, git flow feature pull all repos. Reference feature start script
-alias gffpa="/usr/local/bin/start_feature_branches"
+alias gffpa="/usr/local/bin/pull-branch-all-repos"
+
 #dabnm, delete all branches no merge. Instead of git flow feature or release finish.
 alias dabnm="/usr/local/bin/delete_all_branch_no_merge"
 #lla, list all- even . files like .git etc.
